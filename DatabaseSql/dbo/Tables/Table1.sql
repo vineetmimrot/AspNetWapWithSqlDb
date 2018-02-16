a72de7242cb78a984d9fc2e1fd5e006414a0b496
Vineet Mimrot <vimimrot@microsoft.com>
2018-02-16 18:10:29 +0530

    add database

---
+++ DatabaseSql/dbo/Tables/Table1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table1] (
+    [Id]   INT        NOT NULL,
+    [name] NCHAR (50) NULL,
+    [pwd]  NCHAR (10) NULL,
+    [class] NCHAR(10) NULL, 
+    [companyName] NCHAR(10) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
